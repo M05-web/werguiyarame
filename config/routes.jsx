ca810bdfbd5f247b3aa5c9ad11ddefd3b16b5f8f
M05-web <ahouldtaleb05@gmail.com>
2026-02-11 21:59:21 +0000

    les modifications

--- config/routes.jsx
+++ config/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../src/components/pages/login";
+import Register from "../src/components/pages/register";
 import PrivateRoute from "../src/global/PrivateRoute";
 import MainPage from "../src/components";
 
@@ -9,6 +10,7 @@ const RouteConfig = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route
                 path="/dashboard"
                 element={
